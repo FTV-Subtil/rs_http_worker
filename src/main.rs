d35a377d8e77d72946cc8517f7b926ac42e63861
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-02-26 18:48:00 +0100

    update amqp worker dependency

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
-
 extern crate amqp_worker;
+#[macro_use]
 extern crate log;
 extern crate reqwest;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
 extern crate simple_logger;
 
 use amqp_worker::*;
-use std::env;
 use log::Level;
+use std::env;
 
 mod message;
 
 #[derive(Debug)]
-struct HttpEvent {
-}
+struct HttpEvent {}
 
 impl MessageEvent for HttpEvent {
-  fn process(&self, message: &str) -> Result<u64, MessageError> {
-    message::process(message)
-  }
+    fn process(&self, message: &str) -> Result<u64, MessageError> {
+        message::process(message)
+    }
 }
 
-static HTTP_EVENT: HttpEvent = HttpEvent{};
+static HTTP_EVENT: HttpEvent = HttpEvent {};
 
 fn main() {
-  if let Ok(_)= env::var("VERBOSE") {
-    simple_logger::init_with_level(Level::Debug).unwrap();
-  } else {
-    simple_logger::init_with_level(Level::Warn).unwrap();
-  }
+    if env::var("VERBOSE").is_ok() {
+        simple_logger::init_with_level(Level::Debug).unwrap();
+    } else {
+        simple_logger::init_with_level(Level::Warn).unwrap();
+    }
 
-  start_worker(&HTTP_EVENT);
+    start_worker(&HTTP_EVENT);
 }
--- src/message.rs
+++ src/message.rs
@@ -1,130 +1,75 @@
-
 use amqp_worker::*;
 use reqwest;
 use reqwest::StatusCode;
-use serde_json;
 use std::fs::File;
-use std::path::Path;
 use std::io::prelude::*;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-enum Parameter {
-  #[serde(rename = "string")]
-  StringParam{id: String, default: Option<String>, value: Option<String>},
-  #[serde(rename = "paths")]
-  PathsParam{id: String, default: Option<Vec<String>>, value: Option<Vec<String>>},
-  #[serde(rename = "requirements")]
-  RequirementParam{id: String, default: Option<Requirement>, value: Option<Requirement>},
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Requirement {
-  paths: Option<Vec<String>>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Job {
-  job_id: u64,
-  parameters: Vec<Parameter>
-}
-
-fn get_parameter(params: &Vec<Parameter>, key: &str) -> Option<String> {
-  for param in params.iter() {
-    match param {
-      Parameter::StringParam{id, default, value} => {
-        if id == key {
-          if let Some(ref v) = value {
-            return Some(v.clone())
-          } else {
-            return default.clone()
-          }
-        }
-      },
-      _ => {}
-    }
-  }
-  None
-}
-
-fn check_requirements(params: &Vec<Parameter>) -> Result<(), MessageError> {
-  for param in params.iter() {
-    match param {
-      Parameter::RequirementParam{id, value, ..} => {
-        if id == "requirements" {
-          if let Some(Requirement{paths: Some(paths)}) = value {
-            for ref path in paths.iter() {
-              let p = Path::new(path);
-              if !p.exists() {
-                return Err(MessageError::RequirementsError(format!("Warning: Required file does not exists: {:?}", p)));
-              }
-            }
-          }
-        }
-      },
-      _ => {}
-    }
-  }
-  Ok(())
-}
-
 pub fn process(message: &str) -> Result<u64, MessageError> {
-  println!("{}", message);
-  let parsed: Result<Job, _> = serde_json::from_str(message);
+    let job = job::Job::new(message)?;
+    debug!("reveived message: {:?}", job);
 
-  match parsed {
-    Ok(content) => {
-      println!("{:?}", content);
-
-      match check_requirements(&content.parameters) {
-        Ok(_) => {},
-        Err(message) => { return Err(message); }
-      }
-
-      let url =
-        if let Some(source_path) = get_parameter(&content.parameters, "source_path") {
-          source_path
-        } else {
-          return Err(MessageError::ProcessingError(content.job_id, "missing source path parameter".to_string()));
-        };
-      let filename =
-        if let Some(destination_path) = get_parameter(&content.parameters, "destination_path") {
-          destination_path
-        } else {
-          return Err(MessageError::ProcessingError(content.job_id,"missing destination path parameter".to_string()));
-        };
-
-      let client = reqwest::Client::builder()
-        .build()
-        .map_err(|e| MessageError::ProcessingError(content.job_id, e.to_string()))?;
-
-      let mut response = client.get(url.as_str()).send().map_err(|e| MessageError::ProcessingError(content.job_id, e.to_string()))?;
-
-      let status = response.status();
-
-      if !(status == StatusCode::OK) {
-        println!("ERROR {:?}", response);
-        return Err(MessageError::ProcessingError(content.job_id, "bad response status".to_string()));
-      }
-
-      let mut body: Vec<u8> = vec![];
-      response.copy_to(&mut body).map_err(|e| MessageError::ProcessingError(content.job_id, e.to_string()))?;
-
-      let mut file = File::create(filename.as_str()).map_err(|e| MessageError::ProcessingError(content.job_id, e.to_string()))?;
-      file.write_all(&body).map_err(|e| MessageError::ProcessingError(content.job_id, e.to_string()))?;
-
-      Ok(content.job_id)
-    },
-    Err(msg) => {
-      println!("ERROR {:?}", msg);
-      return Err(MessageError::RuntimeError("bad input message".to_string()));
+    match job.check_requirements() {
+        Ok(_) => {}
+        Err(message) => {
+            return Err(message);
+        }
     }
-  }
+
+    let source_path = job.get_string_parameter("source_path");
+    let destination_path = job.get_string_parameter("destination_path");
+
+    if source_path.is_none() {
+        return Err(MessageError::ProcessingError(
+            job.job_id,
+            "missing source path parameter".to_string(),
+        ));
+    }
+
+    if destination_path.is_none() {
+        return Err(MessageError::ProcessingError(
+            job.job_id,
+            "missing destination path parameter".to_string(),
+        ));
+    }
+
+    let url = source_path.unwrap();
+    let filename = destination_path.unwrap();
+
+    let client = reqwest::Client::builder()
+        .build()
+        .map_err(|e| MessageError::ProcessingError(job.job_id, e.to_string()))?;
+
+    let mut response = client
+        .get(url.as_str())
+        .send()
+        .map_err(|e| MessageError::ProcessingError(job.job_id, e.to_string()))?;
+
+    let status = response.status();
+
+    if status != StatusCode::OK {
+        println!("ERROR {:?}", response);
+        return Err(MessageError::ProcessingError(
+            job.job_id,
+            "bad response status".to_string(),
+        ));
+    }
+
+    let mut body: Vec<u8> = vec![];
+    response
+        .copy_to(&mut body)
+        .map_err(|e| MessageError::ProcessingError(job.job_id, e.to_string()))?;
+
+    let mut file = File::create(filename.as_str())
+        .map_err(|e| MessageError::ProcessingError(job.job_id, e.to_string()))?;
+    file.write_all(&body)
+        .map_err(|e| MessageError::ProcessingError(job.job_id, e.to_string()))?;
+
+    Ok(job.job_id)
 }
 
 #[test]
 fn ack_message_test() {
-  let msg = r#"{
+    let msg = r#"{
     "parameters": [
       {
         "id": "requirements",
@@ -145,13 +90,13 @@ fn ack_message_test() {
     "job_id":690
   }"#;
 
-  let result = process(msg);
-  assert!(result.is_ok());
+    let result = process(msg);
+    assert!(result.is_ok());
 }
 
 #[test]
 fn nack_message_test() {
-  let msg = r#"{
+    let msg = r#"{
     "parameters": [
       {
         "id": "requirements",
@@ -174,7 +119,12 @@ fn nack_message_test() {
     "job_id":690
   }"#;
 
-
-  let result = process(msg);
-  assert_eq!(result, Err(MessageError::RequirementsError("Warning: Required file does not exists: \"/tmp/FiLe_ThAt_$h0uld_N0t_3xist$\"".to_string())));
+    let result = process(msg);
+    assert_eq!(
+        result,
+        Err(MessageError::RequirementsError(
+            "Warning: Required file does not exists: \"/tmp/FiLe_ThAt_$h0uld_N0t_3xist$\""
+                .to_string()
+        ))
+    );
 }
